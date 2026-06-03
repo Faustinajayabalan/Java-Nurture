eeeeb3f11f587e771a91f0b28342e39d92e5f8de
Faustinajayabalan <jayabafaustina@gmail.com>
2026-06-03 09:48:56 +0530

    Added Module 2 ANSI SQL exercises

---
+++ Module2-ANSI-SQL-MySQL/create_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    city VARCHAR(100),
+    registration_date DATE
+);
---
+++ Module2-ANSI-SQL-MySQL/insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Users
+(full_name,email,city,registration_date)
+VALUES
+('Alice Johnson',
+ 'alice@example.com',
+ 'New York',
+ '2024-12-01');
